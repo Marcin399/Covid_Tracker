ed2ad55b4f24a5fdfeb837781e58d1108d5d84ec
Maricn <Inspiremarcin@gmail.com>
2022-01-13 21:22:53 +0100

    Initial Commit

---
+++ Covid_Tracker/Covid_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_TrackerApp.swift
+//  Covid_Tracker
+//
+//  Created by Marcin on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Covid_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Covid_TrackerUITests/Covid_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Covid_TrackerUITestsLaunchTests.swift
+//  Covid_TrackerUITests
+//
+//  Created by Marcin on 13/01/2022.
+//
+
+import XCTest
+
+class Covid_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
